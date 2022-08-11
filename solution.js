dc291afc359a50b1c42073afd83db6c3c49a051e
Rami <103028944+Rami24t@users.noreply.github.com>
2022-08-11 15:25:34 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,2 +1,6 @@
 let [fruit, vegetable, food] = ["banana", "cucumber", ["bread", "cakes", "pizza"]];
 
+let costumes = {"fran": "witch", "lisa": "rabbit"};
+let {fran, lisa} = costumes;
+console.log(fran,lisa);
+
