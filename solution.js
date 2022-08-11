c42ebf097843e652f81baaa179501335463362eb
Rami <103028944+Rami24t@users.noreply.github.com>
2022-08-11 15:39:11 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -4,8 +4,9 @@ let costumes = {"fran": "witch", "lisa": "rabbit"};
 let {fran, lisa} = costumes;
 console.log(fran,lisa);
 
-reginaMusician = {name: 'Regina Spektor', nationality: ' American Russian', occupation: 'singer/songwriter and pianist', genre: 'indie-pop', hit: 'Us'};
+let reginaMusician = {name: 'Regina Spektor', nationality: ' American Russian', occupation: 'singer/songwriter and pianist', genre: 'indie-pop', hit: 'Us'};
 function printMusician (aMusician) {
-console.log(`${aMusician.name} is an ${aMusician.nationality +' '+aMusician.occupation}. The musician sings ${aMusician.genre} and their greatest hit is "${aMusician.hit}".`);
+let {name,nationality,occupation,genre,hit}= aMusician;
+console.log(`${name} is an ${nationality +' '+occupation}. The musician sings ${genre} and their greatest hit is "${hit}".`);
 }
 printMusician(reginaMusician);
